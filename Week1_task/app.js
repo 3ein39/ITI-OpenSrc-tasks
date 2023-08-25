0964540bb46a99b245a9243c43662045935de8ca
HusseinHany <hhany3997@gmail.com>
2023-08-25 17:15:37 +0300

    show valid card for each button click

--- Week1_task/app.js
+++ Week1_task/app.js
@@ -1,5 +1,6 @@
 // change the blur of the 4 buttons on click
 let buttons = document.querySelectorAll("button");
+let cards = document.querySelectorAll(".card");
 buttons.forEach((button) => {
     button.addEventListener("click", () => {
         console.log(button);
@@ -8,6 +9,23 @@ buttons.forEach((button) => {
         setTimeout(() => {
             button.classList.toggle("clicked");
         }, 250);
+
+        // hide all the cards
+        cards.forEach((card) => {
+            card.style.display = "none";
+        })
+
+        // display the card with the clicked button
+        cards.forEach((card) => {
+            if (card.id === button.value) {
+                card.style.display = "block";
+            }
+        })
+
     });
+
+
 })
+
+
 
