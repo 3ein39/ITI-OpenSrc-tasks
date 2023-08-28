fd4f50bcf5505af23f3a655ca1c3618ba212cc82
HusseinHany <hhany3997@gmail.com>
2023-08-28 12:24:52 +0300

    add age

--- Day6/script.js
+++ Day6/script.js
@@ -2,10 +2,12 @@ const nameInput = document.getElementById("name");
 const emailInput = document.getElementById("email");
 const phoneInput = document.getElementById("phone");
 const passwordInput = document.getElementById("password");
+const ageInput = document.getElementById("age");
 
 const nameError = document.getElementById("name-error");
 const emailError = document.getElementById("email-error");
 const phoneError = document.getElementById("phone-error");
+const ageError = document.getElementById("age-error");
 const passwordError = document.getElementById("password-error");
 nameInput.addEventListener("blur", function() {
     if (nameInput.value.trim() === "") {
@@ -24,7 +26,7 @@ emailInput.addEventListener("blur", function() {
     }
 });
 phoneInput.addEventListener("blur", function() {
-    const phonePattern = /^[0-9]{10}$/; // Example: 1234567890
+    const phonePattern = /^(010|011|012|015)[0-9]{8}$/;
     if (!phonePattern.test(phoneInput.value)) {
         phoneError.textContent = "Invalid phone number";
     } else {
@@ -37,5 +39,18 @@ passwordInput.addEventListener("blur", function() {
         passwordError.textContent = "Password must be at least 8 characters long";
     } else {
         passwordError.textContent = "";
+    }
+});
+
+ageInput.addEventListener("blur", function() {
+    const minAge = 18;
+    const maxAge = 24;
+
+    if (ageInput.value === "") {
+        ageError.textContent = "Age is required";
+    } else if (isNaN(ageInput.value) || ageInput.value < minAge || ageInput.value > maxAge) {
+        ageError.textContent = `Age must be between ${minAge} and ${maxAge}`;
+    } else {
+        ageError.textContent = "";
     }
 });
