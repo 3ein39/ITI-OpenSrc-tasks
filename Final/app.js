07199de1e66e21cee999dbef84fb058643f066b0
HusseinHany <hhany3997@gmail.com>
2023-09-02 15:44:15 +0300

    Make nav responsive

--- Final/app.js
+++ Final/app.js
@@ -1,1 +1,7 @@
-console.log("hello")
+// JavaScript to toggle the dropdown menu on small screens
+const menuIcon = document.querySelector(".menu-icon");
+const menu = document.querySelector(".menu");
+
+menuIcon.addEventListener("click", () => {
+    menu.classList.toggle("active");
+});
